4cc7a101209a99b2e99ec79911f2754c29551d81
sushma <sushmal@kesari.in>
2025-04-30 15:39:32 +0530

    newfrontend

---
+++ my-gift/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterModule,HeaderComponent,FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-gift';
+}
---
+++ my-gift/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+
+import { RouterModule,Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+]; 
